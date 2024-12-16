8cdda82b9afe3d31e6f4531ebddf50ac9b131bed
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-12-16 21:34:28 +0600

    category laptop

---
+++ src/components/outlets/home/category/laptops/Laptops.jsx
@@ -0,0 +1,20 @@
+
+import Product from '../Product';
+import {useOutletContext } from 'react-router-dom';
+
+const Laptops = () => {
+    const data = useOutletContext();
+    const products = data.filter(dat => dat.cat == 'Laptops')
+    return (
+        
+        <>
+        <div className="border sm:grid grid-cols-2 md:grid-cols-3 col-span-9 gap-2">
+            {
+            products.map((product) => <Product product={product} key={product.id}/>)
+            }
+        </div>
+        </>
+    );
+};
+
+export default Laptops;
